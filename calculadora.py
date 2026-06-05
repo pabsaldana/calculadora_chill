f9b277035f9642b0665268248a880e86efd16ee9
danborquez-uc <dan.borquez@duocuc.cl>
2026-06-05 09:45:00 -0400

    Update calculadora.py
    la sumita de Catita y Dante

--- calculadora.py
+++ calculadora.py
@@ -1,5 +1,10 @@
 def sumar():
-    print("sumar")
+    num1 = float(input("Ingrese el primer número: "))
+    num2 = float(input("Ingrese el segundo número: "))
+    
+    resultado = num1 + num2
+
+    print(f"El resultado de {num1} + {num2} es: {resultado}")
 
 def restar():
     print("Restar")
