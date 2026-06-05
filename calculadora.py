afc3853076085f4cb990d2391e41182caad46d07
jazzvier1 <sepulvedam.j18@gmail.com>
2026-06-05 10:05:54 -0400

    le pegue una ordenaita profe

--- calculadora.py
+++ calculadora.py
@@ -7,15 +7,15 @@ def sumar():
     print(f"El resultado de {num1} + {num2} es: {resultado}")
 
 def restar():
-    num1=int(input("ingrese su primer numero: "))
-    num2=int(input("Ingrese su segundo numero: "))
+    num1 = int(input("ingrese su primer numero: "))
+    num2 = int(input("Ingrese su segundo numero: "))
     print(f"El resultado de la resta es {num1-num2}")
 
 def dividir():
     print("Dividir")
-    a=float(input("Ingresa el primer numero: "))
-    b=float(input("Ingresa el segundo numero: "))
-    print(f"resultado: {a/b}")
+    a = float(input("Ingresa el primer numero: "))
+    b = float(input("Ingresa el segundo numero: "))
+    print(f"resultado: {a / b}")
 
 
 def multiplicar():
@@ -34,47 +34,52 @@ def raiz():
 def mod():
     print("MOD")
     try:
-        num1=int(input("Ingrese el pimer número"))
-        num2=int(input("Ingrese el sgundo número"))
+        num1 = int(input("Ingrese el pimer número"))
+        num2 = int(input("Ingrese el sgundo número"))
         print(f"El resultado de {num1} mod {num2} es: {num1%num2}")
     except ValueError:
         print("Ingrese un valor numérico")
 
-while True:
-    try:
-
-        print("bienvenido a la calculadora mas completa y epica de la historia")
-        print("1) Sumar")
-        print("2) Restar")
-        print("3) Dividir")
-        print("4) Multiplicar")
-        print("5) Potencia")
-        print("6) Raiz")
-        print("7) MOD")
-        print("0) Salir")
-        opcion=int(input("Ingrse opcion a trabajar: "))
-
-    except ValueError:
-        print("debe ingresar un numero entero")
-        continue
-
-if opcion==1:
-    sumar()
-elif opcion==2:
-    restar()
-elif opcion==3:
-    dividir()
-elif opcion==4:
-    multiplicar()
-elif opcion==5:
-    potencia()
-elif opcion==6:
-    raiz()
-elif opcion==7:
-    mod()
-elif opcion==0:
+def salir():
     print(f"Salir de la fea calculadora")
-    break
-else:
-    print("Opcion no encontrada")
-    
+
+def main():
+    while True:
+        try:
+            print("bienvenido a la calculadora mas completa y epica de la historia")
+            print("1) Sumar")
+            print("2) Restar")
+            print("3) Dividir")
+            print("4) Multiplicar")
+            print("5) Potencia")
+            print("6) Raiz")
+            print("7) MOD")
+            print("0) Salir")
+            opcion = int(input("Ingrese opcion a trabajar: "))
+
+        except ValueError:
+            print("debe ingresar un numero entero")
+            continue
+
+        match opcion:
+            case 1:
+                sumar()
+            case 2:
+                restar()
+            case 3:
+                dividir()
+            case 4:
+                multiplicar()
+            case 5:
+                potencia()
+            case 6:
+                raiz()
+            case 7:
+                mod()
+            case 0:
+                salir()
+                break
+            case _:
+                print("Opcion no encontrada")
+
+main()
